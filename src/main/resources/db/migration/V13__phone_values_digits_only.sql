30d9b30f8b318b1c2220c793571c4f4cf9794565
Yeskendyr <75670312+yesmukhamedov@users.noreply.github.com>
2026-01-30 20:18:43 +0500

    Squash migrations and reseed vocabulary

--- src/main/resources/db/migration/V13__phone_values_digits_only.sql
+++
@@ -1,8 +0,0 @@
-ALTER TABLE phone_values ALTER COLUMN value TYPE VARCHAR(32);
-
-UPDATE phone_values
-SET value = regexp_replace(value, '\\D', '', 'g');
-
-ALTER TABLE phone_values
-    ADD CONSTRAINT chk_phone_values_digits_only
-    CHECK (value ~ '^[0-9]+$');
--- src/main/resources/db/migration/V3__public_edges.sql
+++
@@ -1,5 +0,0 @@
-ALTER TABLE edges ALTER COLUMN from_id DROP NOT NULL;
-
-ALTER TABLE edges DROP CONSTRAINT chk_edges_no_self_loop;
-ALTER TABLE edges
-    ADD CONSTRAINT chk_edges_no_self_loop CHECK (from_id IS NULL OR from_id <> to_id);
